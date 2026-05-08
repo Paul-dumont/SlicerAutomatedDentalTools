f0ff68bc0058e63733a55e799945d24d9831be56
Alexandre Buisson <alexbuis67@outlook.fr>
2026-05-08 09:30:44 -0400

    Update ALI CBCT and IOS utils with errors management and clean logging

--- ALI_IOS/ALI_IOS_utils/agent.py
+++ ALI_IOS/ALI_IOS_utils/agent.py
@@ -1,22 +1,18 @@
 # The Agent class responsible for rendering-based localization
 import torch
-import logging
-import sys
+
 from pytorch3d.renderer import look_at_rotation
 from pytorch3d.structures import Meshes
-
+import logging
+import sys
 # --- LOGGING CONFIGURATION ---
 logger = logging.getLogger("ALI_IOS_agent")
 logger.setLevel(logging.INFO)
-
 logger.propagate = False
-
 if logger.handlers:
     logger.handlers.clear()
-
 console_handler = logging.StreamHandler(sys.stdout)
 console_handler.setLevel(logging.INFO)
-
 formatter = logging.Formatter('%(name)s - %(levelname)s - (%(filename)s:%(lineno)d) - %(message)s')
 console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
